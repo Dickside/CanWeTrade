35d1b179152642548705eca31a90965352ed3bc9
Mat Marsh <giktoruno@gmail.com>
2021-08-14 16:10:30 +0400

    Добавил ViewBag

--- CanWeTrade/Controllers/CarsController.cs
+++ CanWeTrade/Controllers/CarsController.cs
@@ -20,6 +20,7 @@ namespace CanWeTrade.Controllers
 
         public ViewResult List()
         {
+            ViewBag.Category = "Some new";
             var cars = _allCars.Cars;
             return View(cars);
         }
