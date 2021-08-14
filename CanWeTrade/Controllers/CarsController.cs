f5921953bf6050bb174755fc909b8e8346eaae03
Mat Marsh <giktoruno@gmail.com>
2021-08-14 14:17:16 +0400

    Заполнил CarsController, создал конструктор, который устанавливает данные по интерфейсам IAllCars и ICarsCategory, создал функцию ViewResult

--- CanWeTrade/Controllers/CarsController.cs
+++ CanWeTrade/Controllers/CarsController.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using CanWeTrade.Data.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CanWeTrade.Controllers
 {
-    public class CarsController
+    public class CarsController : Controller
     {
+        private readonly IAllCars _allCars;
+        private readonly ICarsCategory _allCategories;
+
+        public CarsController(IAllCars iAllCars, ICarsCategory iCarsCat)
+        {
+            _allCars = iAllCars;
+            _allCategories = iCarsCat;
+        }
+
+        public ViewResult List()
+        {
+            var cars = _allCars.Cars;
+            return View(cars);
+        }
+
     }
 }
